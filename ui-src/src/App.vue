d87a1c06bff3b2c34016f13314d2386e9b7b594c
Devin Brite <devin@dwbrite.com>
2022-01-09 01:10:23 -0500

    wip

--- ui-src/src/App.vue
+++ ui-src/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <Login/>
+  <LoginModal/>
   <MapView/>
 </template>
 
 <script>
 import MapView from './components/MapView.vue'
+import LoginModal from './components/LoginModal.vue'
 
 export default {
   name: 'App',
   components: {
+    LoginModal,
     MapView
   }
 }
--- ui-src/src/components/MapView.vue
+++ ui-src/src/components/MapView.vue
@@ -1,19 +1,47 @@
 <template>
-  <canvas id="map-view"></canvas>
+  <canvas id="map_view"></canvas>
 </template>
 
 <script>
 export default {
   name: 'MapView',
-  components: {
+  mounted() {
+    var canvas = document.getElementById('map_view'),
+        context = canvas.getContext('2d');
+
+    canvas.style.width = screen.width;
+    canvas.style.height = screen.height;
+    canvas.width = screen.width;
+    canvas.height = screen.height;
+
+    function resizeCanvas() {
+      console.log(window.innerHeight);
+      make_base();
+    }
+
+    new ResizeObserver(resizeCanvas).observe(canvas);
+
+
+    make_base();
+
+    function make_base()
+    {
+      let base_image = new Image();
+      let scale = 0.5;
+      base_image.src = '/ui/temp-estar.jpg';
+      base_image.onload = function(){
+        context.drawImage(base_image, 0, 0, base_image.width * scale, base_image.height * scale);
+        console.log(screen.width, screen.height);
+      }
+    }
   }
 }
+
 </script>
 
 <style>
-#map-view {
-  width: 100%;
-  height: 100%;
+#map_view {
   display: block;
+  position: fixed;
 }
 </style>
