aa2f2b3318161e9a0503a866161e69a9e79b014e
Devin <dwbrite@gmail.com>
2022-01-13 03:32:28 -0500

    improve ui routing

--- src/api/v0/users.rs
+++ src/api/v0/users.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
-use axum::{Json, Router};
-use axum::extract::Extension;
-use s3::Bucket;
-use tokio::sync::Mutex;
 use crate::api::v0::meta::auth::UserAuthorization;
-use serde::{Serialize, Deserialize};
+use axum::extract::Extension;
 use axum::routing::*;
+use axum::{Json, Router};
+use s3::Bucket;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub fn router() -> Router {
     let api = Router::new()
@@ -26,26 +26,25 @@ pub struct User {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Users {
-    users: Vec<User>
+    users: Vec<User>,
 }
 
-pub async fn list_users(Extension(bucket): Extension<Arc<Mutex<Bucket>>>) -> Json<Users>{
+pub async fn list_users(Extension(bucket): Extension<Arc<Mutex<Bucket>>>) -> Json<Users> {
     let bucket = bucket.lock().await;
-    let response = bucket.list("players/".to_string(), Some("/".to_string())).await.unwrap();
+    let response = bucket
+        .list("players/".to_string(), Some("/".to_string()))
+        .await
+        .unwrap();
 
-    let mut users = Users {
-        users: vec![]
-    };
+    let mut users = Users { users: vec![] };
 
     // TODO: cache
 
     for prefix in response.first().unwrap().common_prefixes.clone().unwrap() {
-        users.users.push(User { name: prefix.prefix.split("/").collect::<Vec<&str>>()[1].to_string() }) ;
+        users.users.push(User {
+            name: prefix.prefix.split("/").collect::<Vec<&str>>()[1].to_string(),
+        });
     }
-
-    println!("{:?}", response);
-
-
 
     Json(users)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 mod api;
 
-use std::convert::Infallible;
-use std::io;
-use axum::{
-    Router,
-};
 use axum::body::HttpBody;
 use axum::http::{Response, StatusCode};
 use axum::response::{IntoResponse, Redirect};
+use axum::Router;
+use std::convert::Infallible;
+use std::io;
 
 use axum::routing::{get, get_service};
 use tower::ServiceBuilder;
-use tower_http::services::{ServeDir, ServeFile};
 use tower_http::services::fs::ServeFileSystemResponseBody;
+use tower_http::services::{ServeDir, ServeFile};
 use tower_http::trace::TraceLayer;
 
 #[tokio::main]
@@ -23,19 +21,22 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .fallback(get_service(
-            ServiceBuilder::new()
-                .and_then(|response: Response<ServeFileSystemResponseBody>| async move {
-                    let response = if response.status() == StatusCode::NOT_FOUND {
-                        Redirect::to("/".parse().unwrap()).into_response()
-                    }else {
-                        response.map(|body| body.boxed()).into_response()
-                    };
-                    Ok::<_, _>(response)
-                })
-                .service(ServeDir::new("./ui"))
-            // ServeDir::new("./ui")
-        ).handle_error(|_: io::Error| async move {unimplemented!()})
+        .fallback(
+            get_service(
+                ServiceBuilder::new()
+                    .and_then(
+                        |response: Response<ServeFileSystemResponseBody>| async move {
+                            let response = if response.status() == StatusCode::NOT_FOUND {
+                                Redirect::to("/".parse().unwrap()).into_response()
+                            } else {
+                                response.map(|body| body.boxed()).into_response()
+                            };
+                            Ok::<_, _>(response)
+                        },
+                    )
+                    .service(ServeDir::new("./target/ui")), // ServeDir::new("./ui")
+            )
+            .handle_error(|_: io::Error| async move { unimplemented!() }),
         )
         .nest("/api/v0", api::v0::router())
         .layer(TraceLayer::new_for_http());
